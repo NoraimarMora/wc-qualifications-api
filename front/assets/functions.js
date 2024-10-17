d662e91d86de8b9616ce6069e84fc1d12f1eee59
Noraimar Mora <noraimar.mora@gmail.com>
2024-10-17 13:06:05 -0400

    Update standings 15-10-2024

--- front/assets/functions.js
+++ front/assets/functions.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
     $("#country-title").on("click", function() {
-        if (!$(".countries").hasClass("hidden"))
+        if (!$(".countries").hasClass("hidden")) {
             $(".countries").addClass("hidden");
-        else
+            $("#country-title i").css("transform", "rotate(0deg)")
+        } else {
             $(".countries").removeClass("hidden");
+            $("#country-title i").css("transform", "rotate(180deg)")
+        }
 
         if (!$(".leagues").hasClass("hidden"))
             $(".leagues").addClass("hidden");
@@ -13,13 +16,22 @@ $(document).ready(function () {
 
         if (!$(".standings").hasClass("hidden"))
             $(".standings").addClass("hidden");
+
+        if (!$(".news").hasClass("hidden"))
+            $(".news").addClass("hidden");
+
+        if (!$(".ranking").hasClass("hidden"))
+            $(".ranking").addClass("hidden");
     });
 
     $("#league-title").on("click", function() {
-        if (!$(".leagues").hasClass("hidden"))
+        if (!$(".leagues").hasClass("hidden")) {
             $(".leagues").addClass("hidden");
-        else
+            $("#league-title i").css("transform", "rotate(0deg)")
+        } else {
             $(".leagues").removeClass("hidden");
+            $("#league-title i").css("transform", "rotate(180deg)")
+        }
 
         if (!$(".countries").hasClass("hidden"))
             $(".countries").addClass("hidden");
@@ -29,13 +41,22 @@ $(document).ready(function () {
 
         if (!$(".standings").hasClass("hidden"))
             $(".standings").addClass("hidden");
+
+        if (!$(".news").hasClass("hidden"))
+            $(".news").addClass("hidden");
+
+        if (!$(".ranking").hasClass("hidden"))
+            $(".ranking").addClass("hidden");
     });
 
     $("#match-title").on("click", function() {
-        if (!$(".matches").hasClass("hidden"))
+        if (!$(".matches").hasClass("hidden")) {
             $(".matches").addClass("hidden");
-        else
+            $("#match-title i").css("transform", "rotate(0deg)")
+        } else {
             $(".matches").removeClass("hidden");
+            $("#match-title i").css("transform", "rotate(180deg)")
+        }    
 
         if (!$(".countries").hasClass("hidden"))
             $(".countries").addClass("hidden");
@@ -45,14 +66,23 @@ $(document).ready(function () {
 
         if (!$(".standings").hasClass("hidden"))
             $(".standings").addClass("hidden");
+
+        if (!$(".news").hasClass("hidden"))
+            $(".news").addClass("hidden");
+
+        if (!$(".ranking").hasClass("hidden"))
+            $(".ranking").addClass("hidden");
     });
 
     $("#standing-title").on("click", function() {
-        if (!$(".standings").hasClass("hidden"))
+        if (!$(".standings").hasClass("hidden")) {
             $(".standings").addClass("hidden");
-        else
+            $("#standing-title i").css("transform", "rotate(0deg)")
+        } else {
             $(".standings").removeClass("hidden");
-        
+            $("#standing-title i").css("transform", "rotate(180deg)")
+        }
+
         if (!$(".countries").hasClass("hidden"))
             $(".countries").addClass("hidden");
 
@@ -61,5 +91,61 @@ $(document).ready(function () {
 
         if (!$(".leagues").hasClass("hidden"))
             $(".leagues").addClass("hidden");
+
+        if (!$(".news").hasClass("hidden"))
+            $(".news").addClass("hidden");
+
+        if (!$(".ranking").hasClass("hidden"))
+            $(".ranking").addClass("hidden");
+    });
+
+    $("#news-title").on("click", function() {
+        if (!$(".news").hasClass("hidden")) {
+            $(".news").addClass("hidden");
+            $("#news-title i").css("transform", "rotate(0deg)")
+        } else {
+            $(".news").removeClass("hidden");
+            $("#news-title i").css("transform", "rotate(180deg)")
+        }
+
+        if (!$(".countries").hasClass("hidden"))
+            $(".countries").addClass("hidden");
+
+        if (!$(".matches").hasClass("hidden"))
+            $(".matches").addClass("hidden");
+
+        if (!$(".leagues").hasClass("hidden"))
+            $(".leagues").addClass("hidden");
+
+        if (!$(".standings").hasClass("hidden"))
+            $(".standings").addClass("hidden");
+
+        if (!$(".ranking").hasClass("hidden"))
+            $(".ranking").addClass("hidden");
+    });
+
+    $("#ranking-title").on("click", function() {
+        if (!$(".ranking").hasClass("hidden")) {
+            $(".ranking").addClass("hidden");
+            $("#ranking-title i").css("transform", "rotate(0deg)")
+        } else {
+            $(".ranking").removeClass("hidden");
+            $("#ranking-title i").css("transform", "rotate(180deg)")
+        }
+
+        if (!$(".countries").hasClass("hidden"))
+            $(".countries").addClass("hidden");
+
+        if (!$(".matches").hasClass("hidden"))
+            $(".matches").addClass("hidden");
+
+        if (!$(".leagues").hasClass("hidden"))
+            $(".leagues").addClass("hidden");
+
+        if (!$(".news").hasClass("hidden"))
+            $(".news").addClass("hidden");
+
+        if (!$(".standings").hasClass("hidden"))
+            $(".standings").addClass("hidden");
     });
 });
